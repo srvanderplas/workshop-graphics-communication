8ff423e1aa519fd0818372d771373613cf0f3fe8
Susan Vanderplas <srvanderplas@gmail.com>
2024-05-30 14:52:15 -0500

    Updates - Section 04 content.

---
+++ code/eda-code.py
@@ -0,0 +1,9 @@
+import pandas as pd
+# Plotting libraries
+import matplotlib.pyplot as plt
+import seaborn as sns
+import seaborn.objects as so
+# Missing data libraries
+import missingno as msno
+
+new_data = pd.read_csv("https://raw.githubusercontent.com/srvanderplas/workshop-graphics-communication/main/data/new_data.csv")
